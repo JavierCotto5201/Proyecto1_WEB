dcd9b4d5e384a545fc79d1e1da0b3d2583a89b89
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-04-16 13:01:28 -0600

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": ["error"],
+        "no-extra-semi":"error",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "space-before-blocks": ["error"],
+        "parser": "babel-eslint",
+        "semi":0
+    }
+};
